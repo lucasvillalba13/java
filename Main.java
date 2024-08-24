4844e6d9712e8c622129640f9e6685e763e26364
lucas villalba <lucasvillalba13@gmail.com>
2024-08-24 20:03:00 -0300

    uso de operadores logicos

--- Main.java
+++ Main.java
@@ -1,27 +1,39 @@
 package d;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		String day = "fobal";
+		/*int temp = 20;
 		
-		switch (day) {
-			case "Sunday": System.out.println("it is sunday!");
-			break;
-			case "Monday": System.out.println("it is monday!");
-			break;
-			case "Tuesday": System.out.println("it is tuesday!");
-			break;
-			case "Wednesday": System.out.println("it is wednesday!");
-			break;
-			case "Thursday": System.out.println("it is thursday!");
-			break;
-			case "Friday": System.out.println("it is friday!");
-			break;
-			case "Saturday": System.out.println("it is saturday!");
-			break;
-			default: System.out.println("that is not a day");
+		if (temp <= 15) {
+			System.out.println("hace frio");
+		} else if (temp>=28 ) {
+			System.out.println("hace calooooorrrr");
+		} else if (temp > 20 && temp < 27);{
+			System.out.println("ta hermoso afuera");
+		} else {
+			System.out.println("no salen ni las iguanas");
 		}
+		*/
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("you are playing a game! Press 'Q' or 'q' if you want to leave!");
+		String response = scanner.next();
+		
+		//if (response.equals("q") || response.equals ("Q")) {
+		//System.out.println("you have quited successfully");
+		//} else {
+		//	System.out.println("you are still plying the game");
+		//}
+		
+		if (!response.equals("q") && !response.equals ("Q")) {
+			System.out.println("you are still plying the game");
+		} else {
+			System.out.println("you have quited successfully");
+		}
+			
 	}
 }

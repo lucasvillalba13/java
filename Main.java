4bc76021f5f1416130e430a0af10ea90e1770807
lucas villalba <lucasvillalba13@gmail.com>
2024-08-25 16:42:13 -0300

    metodos de string

--- Main.java
+++ Main.java
@@ -3,54 +3,23 @@ package d;
 public class Main {
 
 	public static void main(String[] args) {
-
-		// arrays
 		
-	//String[] cars = {"Camaro","Fiat", "Mercedes"};	
-	//System.out.println(cars[0]);
-	
-	//String[] materias = new String[4];
-			
-	//materias[0] = "teorias del aprendizaje";
-	//materias[1] = "seminario";
-	//materias[2] = "epistemologia";
-	//materias[3] = "didactica";
-	
-	//for (int i=0; i<materias.length; i++) {
-		//System.out.println(materias[i]);
-		//}
+		//metodos de string
 		
-		// 2D arrays 
+		String name = "lucas";
 		
-		/*String[][] cars = new String[3][3];
+		//boolean result = name.equalsIgnoreCase("Lucas");
+		//int result = name.length();
+		//char result = name.charAt(0);
+		//int result = name.indexOf("u");
+		//boolean result = name.isEmpty();
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		//String result = name.trim();
+		String result = name.replace('u', 'o');
 		
-		cars[0][0] = "208";
-		cars[0][1] = "corolla";
-		cars[0][2] = "corolla";
-		cars[1][0] = "palio";
-		cars[1][1] = "hilux";
-		cars[1][2] = "corolla";
-		cars[2][0] = "vento";
-		cars[2][1] = "Q5";
-		cars[2][2] = "corolla";
+		System.out.println(result);
 		
-		for(int i = 0; i<cars.length; i++) {
-			System.out.println();
-			for(int j = 0; j<cars[i].length;j++) {
-				System.out.print(cars[i][j]+ " ");
-			}*/
-		
-			String[][] cars = { {"208","corolla","corolla"},
-								{"palio","hilux","corolla"},
-								{"vento","Q5","corolla"}
-						};
-			
-			for(int i = 0; i<cars.length; i++) {
-				System.out.println();
-				for(int j = 0; j<cars[i].length;j++) {
-					System.out.print(cars[i][j]+ " ");
-				}
-			}
 	}
 }
 			

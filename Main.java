bb6704648445a315716034238bec370b89f6e311
lucas villalba <lucasvillalba13@gmail.com>
2024-08-25 16:31:26 -0300

    arrays y arrays 2d

--- Main.java
+++ Main.java
@@ -1,32 +1,56 @@
 package d;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		// nested loops
+		// arrays
 		
-		Scanner scanner = new Scanner(System.in);
+	//String[] cars = {"Camaro","Fiat", "Mercedes"};	
+	//System.out.println(cars[0]);
+	
+	//String[] materias = new String[4];
+			
+	//materias[0] = "teorias del aprendizaje";
+	//materias[1] = "seminario";
+	//materias[2] = "epistemologia";
+	//materias[3] = "didactica";
+	
+	//for (int i=0; i<materias.length; i++) {
+		//System.out.println(materias[i]);
+		//}
 		
-		int rows;
-		int columns;
-		String symbols = "";
+		// 2D arrays 
 		
-		System.out.println("Enter number of rows");
-		rows = scanner.nextInt();
+		/*String[][] cars = new String[3][3];
 		
-		System.out.println("Enter number of columns");
-		columns = scanner.nextInt();
+		cars[0][0] = "208";
+		cars[0][1] = "corolla";
+		cars[0][2] = "corolla";
+		cars[1][0] = "palio";
+		cars[1][1] = "hilux";
+		cars[1][2] = "corolla";
+		cars[2][0] = "vento";
+		cars[2][1] = "Q5";
+		cars[2][2] = "corolla";
 		
-		System.out.println("Enter symbol to use");
-		symbols = scanner.next();
-		
-		for (int i = 1; i<= rows; i++) {
+		for(int i = 0; i<cars.length; i++) {
 			System.out.println();
-			for(int j = 1; j <= columns; j++) {
-				System.out.print(symbols);
+			for(int j = 0; j<cars[i].length;j++) {
+				System.out.print(cars[i][j]+ " ");
+			}*/
+		
+			String[][] cars = { {"208","corolla","corolla"},
+								{"palio","hilux","corolla"},
+								{"vento","Q5","corolla"}
+						};
+			
+			for(int i = 0; i<cars.length; i++) {
+				System.out.println();
+				for(int j = 0; j<cars[i].length;j++) {
+					System.out.print(cars[i][j]+ " ");
+				}
 			}
-		}
 	}
 }
+			

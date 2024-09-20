7584151e82bc21aa92e781e5a9cbe8d346d7af18
lucas villalba <lucasvillalba13@gmail.com>
2024-09-20 00:34:27 -0300

    exception handling

--- Main.java
+++ Main.java
@@ -1,33 +1,56 @@
 package d;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Garage garage = new Garage(10);
+		Scanner scanner = new Scanner(System.in);
 
-		Vehicle car1 = new Car("NJF042");
-		Vehicle bici = new Bycicle("MEM912");
-		Vehicle camion = new Truck("ACM1PT");
+		try{
+			System.out.println("enter a number to divide");
+			int x = scanner.nextInt();
 
-		//array de objetos utilizando polimorfismo
-		Vehicle[] polimor = {car1,bici,camion};
+			System.out.println("enter a number to divide by");
+			int y = scanner.nextInt();
 
-		garage.addVehicle(car1);
-		garage.addVehicle(bici);
-
-
-		try {
-			Thread.sleep(60000); // Esperar 60 segundos (1 minuto)
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			int z = x/y;
+			System.out.println("el resultado es: " + z);
+		} catch (ArithmeticException e){
+			System.out.println("un numero no se puede dividir por cero");
+		} catch (InputMismatchException e){
+			System.out.println("pone un numero flaco");
+		} finally {
+			scanner.close();
 		}
 
-		long timeStored = garage.calculateStoredTime("NJF042");
-		System.out.println("Time stored (in minutes): " + timeStored);
 
-		garage.removeVehicle("NJF042");
+//
+//		Garage garage = new Garage(10);
+//
+//		Vehicle car1 = new Car("NJF042");
+//		Vehicle bici = new Bycicle("MEM912");
+//		Vehicle camion = new Truck("ACM1PT");
+//
+//		//array de objetos utilizando polimorfismo
+//		Vehicle[] polimor = {car1,bici,camion};
+//
+//		garage.addVehicle(car1);
+//		garage.addVehicle(bici);
+//
+//
+//		try {
+//			Thread.sleep(60000); // Esperar 60 segundos (1 minuto)
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//
+//		long timeStored = garage.calculateStoredTime("NJF042");
+//		System.out.println("Time stored (in minutes): " + timeStored);
+//
+//		garage.removeVehicle("NJF042");
 
 //		Professor profe = new Professor();
 //

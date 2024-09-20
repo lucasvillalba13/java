9605295993900afcd6866b20cc39c4720a9336db
lucas villalba <lucasvillalba13@gmail.com>
2024-09-19 23:57:15 -0300

    polimorfismo

--- Main.java
+++ Main.java
@@ -5,28 +5,32 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		Garage garage = new Garage(10);
-//
-//		Vehicle car1 = new Car("NJF042");
-//		Vehicle bici = new Bycicle("MEM912");
-//
-//		garage.addVehicle(car1);
-//		garage.addVehicle(bici);
-//
-//
-//		try {
-//			Thread.sleep(60000); // Esperar 60 segundos (1 minuto)
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//
-//		long timeStored = garage.calculateStoredTime("NJF042");
-//		System.out.println("Time stored (in minutes): " + timeStored);
-//
-//		garage.removeVehicle("NJF042");
+		Garage garage = new Garage(10);
 
-		Professor profe = new Professor();
+		Vehicle car1 = new Car("NJF042");
+		Vehicle bici = new Bycicle("MEM912");
+		Vehicle camion = new Truck("ACM1PT");
 
-		profe.Reading();
+		//array de objetos utilizando polimorfismo
+		Vehicle[] polimor = {car1,bici,camion};
+
+		garage.addVehicle(car1);
+		garage.addVehicle(bici);
+
+
+		try {
+			Thread.sleep(60000); // Esperar 60 segundos (1 minuto)
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		long timeStored = garage.calculateStoredTime("NJF042");
+		System.out.println("Time stored (in minutes): " + timeStored);
+
+		garage.removeVehicle("NJF042");
+
+//		Professor profe = new Professor();
+//
+//		profe.Reading();
     }
 }			
---
+++ Truck.java
@@ -0,0 +1,7 @@
+package d;
+
+public class Truck extends Vehicle{
+    public Truck(String licensePlate) {
+        super(licensePlate, "Truck");
+    }
+}

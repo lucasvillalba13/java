0b24e6c9c62852670fbf9290ef54489c6157bc62
lucas villalba <lucasvillalba13@gmail.com>
2024-09-21 23:49:48 -0300

    file class

--- Main.java
+++ Main.java
@@ -1,59 +1,18 @@
 package d;
+import java.io.File;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		File file = new File("file.txt");
+		// if the file exist in any other path within the computer you must write down the entire path
 
-		try{
-			System.out.println("enter a number to divide");
-			int x = scanner.nextInt();
-
-			System.out.println("enter a number to divide by");
-			int y = scanner.nextInt();
-
-			int z = x/y;
-			System.out.println("el resultado es: " + z);
-		} catch (ArithmeticException e){
-			System.out.println("un numero no se puede dividir por cero");
-		} catch (InputMismatchException e){
-			System.out.println("pone un numero flaco");
-		} finally {
-			scanner.close();
+		if(file.exists()){
+			System.out.println("this file.txt exists");
+		} else {
+			System.out.println("this file.txt does not exist");
 		}
-
-
-//
-//		Garage garage = new Garage(10);
-//
-//		Vehicle car1 = new Car("NJF042");
-//		Vehicle bici = new Bycicle("MEM912");
-//		Vehicle camion = new Truck("ACM1PT");
-//
-//		//array de objetos utilizando polimorfismo
-//		Vehicle[] polimor = {car1,bici,camion};
-//
-//		garage.addVehicle(car1);
-//		garage.addVehicle(bici);
-//
-//
-//		try {
-//			Thread.sleep(60000); // Esperar 60 segundos (1 minuto)
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//
-//		long timeStored = garage.calculateStoredTime("NJF042");
-//		System.out.println("Time stored (in minutes): " + timeStored);
-//
-//		garage.removeVehicle("NJF042");
-
-//		Professor profe = new Professor();
-//
-//		profe.Reading();
     }
 }			
